78f35eea06a86ce75a95a6b2b5b35b4e6faeac17
chayanjana1 <107670948+chayanjana1@users.noreply.github.com>
2022-08-21 20:04:29 +0530

    Add files via upload

---
+++ batchsystem/NoCourseFoundException.java
@@ -0,0 +1,10 @@
+package batchsystem;
+//create NoCourseFoundException for thorw exception
+public class NoCourseFoundException extends RuntimeException {
+	 public NoCourseFoundException(String name)
+	 {
+		 super(name);
+	 }
+
+}
+
