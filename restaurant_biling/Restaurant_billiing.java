0e0db5f2f23b4cb1ce65bbdb7fc69810e90a6589
chayanjana1 <107670948+chayanjana1@users.noreply.github.com>
2022-09-02 14:48:01 +0530

    Add files via upload

---
+++ restaurant_biling/Restaurant_billiing.java
@@ -0,0 +1,38 @@
+package restaurant_biling;
+
+import java.util.Scanner;
+
+public class Restaurant_billiing {
+
+	public static void main(String[] args)
+	{
+		String ch;
+		int id;
+		Scanner sc=new Scanner(System.in);
+		//using map
+		System.out.println("1.Biriyani Combo    Rs.150");
+		System.out.println("2.Chineese Combo    Rs.250");
+		System.out.println("3.Italian Combo     Rs.450");
+		System.out.println("4.Bada Platter      Rs.200");
+		do {
+			System.out.println("Enter food id");
+			id=sc.nextInt();
+			switch(id) {
+			case 1:RestaurantMangmentSystem.oderTaking("Biriyani Combo", 150);
+			break;
+			case 2:RestaurantMangmentSystem.oderTaking("Chineese Combo", 250);
+			break;
+			case 3:RestaurantMangmentSystem.oderTaking("Italian Combo", 450);
+			break;
+			case 4:RestaurantMangmentSystem.oderTaking("Bada Platter", 200);
+			break;
+			default:System.out.println("Wrong input");
+			}
+			System.out.print("Continue billing press y/n:");
+			ch=sc.next();
+			System.out.println("ch:"+ch);
+			
+		}while(ch.equalsIgnoreCase("y"));
+		RestaurantMangmentSystem.billing();
+	}
+}
